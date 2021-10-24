11f652ed94bf567e42d662cea060edf602fab947
CARRAU KRIZAJ, FRANCO <35275323@comunidadort.edu.ar>
2021-10-24 12:10:21 -0300

    inicial. creacion proyecto

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={require('./assets/imagenes/background.jpg')} resizeMode="cover" style={styles.background}>
+      
+{/*       <Text style={styles.titulo}> Cinemagram </Text> */}
+
+      <View style={styles.buttonContainer}> 
+        <View style={styles.button}>
+          <Button title='Iniciar sesión' color='#790c5a' /* onPress={} *//>
+        </View>
+        <View style={styles.button}>
+          <Button title='Registrarme' color='#e6739f' /* onPress={} *//>
+        </View>
+      </View>
+
+      
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +28,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+/*     backgroundColor: '#c3bef0', */
+    alignItems: 'center'
   },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingBottom: 100
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignSelf: 'center',
+    paddingTop: 150
+  },
+  background:{
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  button: {
+    paddingHorizontal: 10
+  }
 });
